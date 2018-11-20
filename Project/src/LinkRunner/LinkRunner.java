09b7982477de1fb8462267823449a546a77639fd
김상언 <gggomksy@naver.com>
2018-11-20 15:24:48 +0900

    링크 저장받은거 실행하는 것 구혀

--- Project/src/LinkRunner/LinkRunner.java
+++ Project/src/LinkRunner/LinkRunner.java
@@ -12,15 +12,39 @@ public class LinkRunner
 		
 		while(true)
 		{
-			System.out.print("저장할 파일명과 링크를 입력하세요>>");
-			String name = scanner.next();
-			if(name.equals("stop"))
-				continue;
+			System.out.println("1.실행하기\n2.저장하기");
+			System.out.print("실행할 옵션을 선택하세요>>");
+			int n = scanner.nextInt();
 			
-			String link = scanner.nextLine();
-			link = link.trim();
+			if(n==1)
+			{
+				System.out.print("실행할 프로그램을 입력하세요>>");
+				String s = scanner.next();
+				if(h.containsKey(s))
+				{	
+					try	
+					{
+						Process oProcess = new ProcessBuilder(h.get(s)).start();
+					}
+					catch (IOException e)
+					{
+				  	   System.err.println("에러! 외부 명령 실행에 실패했습니다.\n" + e.getMessage());
+				  	   System.exit(-1);}
+					}
+				}
 			
-			h.put(name, link);
+			if(n==2)
+			{
+				System.out.print("저장할 파일명과 링크를 입력하세요>>");
+				String name = scanner.next();
+				if(name.equals("stop"))
+					continue;
+			
+				String link = scanner.nextLine();
+				link = link.trim();
+			
+				h.put(name, link);
+			}
 		}
 	}
 }
